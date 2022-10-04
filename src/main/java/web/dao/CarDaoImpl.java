d6c8ff1be4584b82b77d32b092696cc3c291da17
negrulya <sunohara29@yandex.ru>
2022-10-04 15:28:51 +0300

    done

--- src/main/java/web/dao/CarDaoImpl.java
+++ src/main/java/web/dao/CarDaoImpl.java
@@ -4,13 +4,14 @@ import org.springframework.stereotype.Repository;
 import web.model.Car;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Repository
 public class CarDaoImpl implements CarDao{
 
-    private static final List<Car> cars = new ArrayList<>();
+    public static final List<Car> cars = new ArrayList<>();
 
     static {
         cars.add(new Car("Bebramobile", 2021, "Yegor"));
@@ -18,14 +19,5 @@ public class CarDaoImpl implements CarDao{
         cars.add(new Car("AutoVAZ", 7, "Tequila"));
         cars.add(new Car("GAZ Volga", 290, "Kras"));
         cars.add(new Car("Batmobile", 134, "Batman"));
-    }
-
-    @Override
-    public List<Car> getCars(int count) {
-        if (count == 0) {
-            return cars;
-        }
-
-        return cars.stream().limit(count).collect(Collectors.toList());
     }
 }
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -3,18 +3,29 @@ package web.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import web.dao.CarDao;
+import web.dao.CarDaoImpl;
 import web.model.Car;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CarServiceImpl implements CarService{
 
-    @Autowired
-    private CarDao carDao;
+    public final CarDao carDao;
+    public List<Car> cars = CarDaoImpl.cars;
+
+    public CarServiceImpl(CarDao carDao) {
+        this.carDao = carDao;
+    }
+
 
     @Override
     public List<Car> getCars(int count) {
-        return carDao.getCars(count);
+        if ((count == 0) || (count >= 5)) {
+            return cars;
+        }
+        return cars.stream().limit(count).collect(Collectors.toList());
     }
 }
+
